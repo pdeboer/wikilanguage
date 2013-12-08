8f0492469e12094efcef3c30afd654943041ab48
Patrick de Boer <pdeboer@mit.edu>
2013-12-08 14:37:11 -0500

    sql scripts

---
+++ src/test/helper.sql
@@ -0,0 +1,2 @@
+select name from categories c inner join people2categories p2c on c.id=p2c.`category` and person=675;
+
--- src/test/setbirthdays.sql
+++ src/test/setbirthdays.sql
@@ -4,4 +4,4 @@ update people set year_to = year_from + 100 where year_to is null;
 
 -- populate years table
 update years
-  set people_alive = (select count(*) from people where year_from is not null and years.id between year_from and year_to )
+  set people_alive = (select count(*) from people where year_from is not null and years.id between year_from and year_to );
