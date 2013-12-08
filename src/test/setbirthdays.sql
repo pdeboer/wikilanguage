e534eab59680b627e531cb2642def7213931c499
Patrick de Boer <pdeboer@mit.edu>
2013-12-07 22:17:07 -0500

    calc histogramm of alive people count over time

---
+++ src/test/setbirthdays.sql
@@ -0,0 +1,7 @@
+-- populate table with expected age for people where we know only half the story
+update people set year_from = year_to - 100 where year_from is null;
+update people set year_to = year_from + 100 where year_to is null;
+
+-- populate years table
+update years
+  set people_alive = (select count(*) from people where year_from is not null and years.id between year_from and year_to )
