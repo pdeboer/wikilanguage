2675b3244574c073655be766d995b0587743951f
Patrick de Boer <pdeboer@mit.edu>
2014-01-17 17:34:52 -0500

    todouble

--- src/main/scala/edu/mit/cci/wikilanguage/main/BetweennessCalculator.scala
+++ src/main/scala/edu/mit/cci/wikilanguage/main/BetweennessCalculator.scala
@@ -13,7 +13,7 @@ object BetweennessCalculator extends App {
 
 	val calculator = new BetweennessByYearCalculator()
 
-	val exec = Executors.newFixedThreadPool(2)
+	val exec = Executors.newFixedThreadPool(1)
 
 	DAO.getYears().foreach(year => {
 		exec.submit(new Runnable {
